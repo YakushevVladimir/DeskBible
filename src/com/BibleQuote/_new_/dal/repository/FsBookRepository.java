7d2925e99492a3bf68b7e5e075c215b30febcda3
SergeyUrsul <serg.ursul@gmail.com>
2011-12-16 18:28:30 +0400

    Refactoring_3

--- src/com/BibleQuote/_new_/dal/repository/FsBookRepository.java
+++ src/com/BibleQuote/_new_/dal/repository/FsBookRepository.java
@@ -1,41 +1,83 @@
 package com.BibleQuote._new_.dal.repository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.LinkedHashMap;
 
 import com.BibleQuote._new_.dal.FsLibraryContext;
 import com.BibleQuote._new_.models.Book;
+import com.BibleQuote._new_.models.FsBook;
+import com.BibleQuote._new_.models.FsModule;
+import com.BibleQuote.exceptions.CreateModuleErrorException;
 
-public class FsBookRepository implements IBookRepository<String> {
+public class FsBookRepository implements IBookRepository<FsModule, FsBook> {
 	
-	FsLibraryContext context;
-
+	private FsLibraryContext context;
+	
     public FsBookRepository(FsLibraryContext context) {
     	this.context = context;
     }
     
+    
 	@Override
-	public Collection<Book> getBooks(String moduleId) {
-		return null;
+	public Collection<FsBook> loadBooks(FsModule module) {
+		if (!context.isModuleLoaded(module)) {
+			context.moduleSet.put(module.ShortName, module);
+		}
+		
+		module.Books = context.bookSet = new LinkedHashMap<String, Book>();
+		BufferedReader reader = null;
+		try {
+			reader = context.getModuleReader(module); 
+			context.fillBooks(module, reader);
+		} catch (CreateModuleErrorException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace(); 
+			}
+		}
+		
+		return context.getBookList(module.Books); 	
 	}
-
+	
+	
 	@Override
-	public Book getBookById(String bookId) {
-		return null;
+	public Collection<FsBook> getBooks(FsModule module) {
+		if (!context.isModuleLoaded(module)) {
+			context.moduleSet.put(module.ShortName, module);
+		}
+		
+		return context.getBookList(module.Books); 
 	}
 
 	
 	@Override
-	public void insertBook(Book book) {
+	public FsBook getBookByName(FsModule module, String bookName) {
+		if (!context.isModuleLoaded(module)) {
+			context.moduleSet.put(module.ShortName, module);
+		}
+		
+		return (FsBook)module.Books.get(bookName);
 	}
 
 	
 	@Override
-	public void deleteBook(String bookId) {
+	public void insertBook(FsBook book) {
 	}
+
 	
+	@Override
+	public void deleteBook(FsBook book) {
+	}
+
 
 	@Override
-	public void updateBook(Book book) {
+	public void updateBook(FsBook book) {
 	}
+
 
 }
