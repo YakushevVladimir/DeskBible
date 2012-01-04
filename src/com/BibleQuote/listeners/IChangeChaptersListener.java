8670f7a9437e2bbc20fd8926bc22a7449b43393d
SergeyUrsul <serg.ursul@gmail.com>
2012-01-04 18:40:01 +0400

    Delete

--- src/com/BibleQuote/listeners/IChangeChaptersListener.java
+++
@@ -1,9 +0,0 @@
-package com.BibleQuote.listeners;
-
-public interface IChangeChaptersListener {
-	public enum ChangeCode {
-		ChapterAdded
-	}
-	
-	public void onChangeChapters(ChangeChaptersEvent event);
-}
