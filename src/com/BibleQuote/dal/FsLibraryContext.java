d0a0319a6959369587f0de377c8b89f9a734f163
Vladimir Yakushev <ru.phoenix@gmail.com>
2013-11-10 23:53:00 +0600

    Acceleration search

--- src/com/BibleQuote/dal/FsLibraryContext.java
+++ src/com/BibleQuote/dal/FsLibraryContext.java
@@ -35,7 +35,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 public class FsLibraryContext extends LibraryContext {
 	private final String TAG = "FsLibraryContext";
