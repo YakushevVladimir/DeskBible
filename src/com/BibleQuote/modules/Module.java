cf5b6b2d847b03f08f9a9d16588402fb2e38773a
Vladimir Yakushev <ru.phoenix@gmail.com>
2014-06-18 18:20:55 +0600

    Refactoring

--- src/com/BibleQuote/modules/Module.java
+++ src/com/BibleQuote/modules/Module.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Scripture Software (http://scripturesoftware.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.BibleQuote.modules;
 
 import java.io.Serializable;
@@ -9,49 +25,46 @@ import java.util.Map;
  */
 public abstract class Module implements Serializable {
 
-	private static final long serialVersionUID = -499369158022814559L;
+    private static final long serialVersionUID = -499369158022814559L;
+    public String ShortName = "";
+    public String ChapterSign = "";
+    public String VerseSign = "";
+    public String HtmlFilter = "";
+    public boolean ChapterZero = false;
+    public boolean containsStrong = false;
+    public boolean isBible = false;
+    public String defaultEncoding = "utf-8";
+    public String language = "ru_RU";
+    public Map<String, Book> Books = new LinkedHashMap<String, Book>();    // to lazy loading on demand
+    private String Name = "";
 
-	private String Name = "";
-	public String ShortName = "";
-	public String ChapterSign = "";
-	public String VerseSign = "";
-	public String HtmlFilter = "";
-	public boolean ChapterZero = false;
-	public boolean containsStrong = false;
-	public boolean isBible = false;
-	public String defaultEncoding = "utf-8";
-	public String language = "ru_RU";
+    // public String Categories = "";
+    // public String Copyright = "";
+    // public boolean containsOT = false;
+    // public boolean containsNT = false;
+    // public boolean containsAP = false;
 
+    public String toString() {
+        return this.Name;
+    }
 
-	public Map<String, Book> Books = new LinkedHashMap<String, Book>();    // to lazy loading on demand
+    public abstract String getID();
 
-	// public String Categories = "";
-	// public String Copyright = "";
-	// public boolean containsOT = false;
-	// public boolean containsNT = false;
-	// public boolean containsAP = false;
+    public abstract String getDataSourceID();
 
-	public String toString() {
-		return this.Name;
-	}
+    public String getName() {
+        return Name;
+    }
 
-	public abstract String getID();
+    public void setName(String name) {
+        Name = name;
+    }
 
-	public abstract String getDataSourceID();
-
-	public String getName() {
-		return Name;
-	}
-
-	public void setName(String name) {
-		Name = name;
-	}
-
-	public String getLanguage() {
-		if (language == null || language.indexOf("-") == -1) {
-			return "ru";
-		} else {
-			return language.substring(0, language.indexOf("-")).toLowerCase();
-		}
-	}
+    public String getLanguage() {
+        if (language == null || !language.contains("-")) {
+            return "ru";
+        } else {
+            return language.substring(0, language.indexOf("-")).toLowerCase();
+        }
+    }
 }
