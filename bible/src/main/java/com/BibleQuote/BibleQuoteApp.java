4e0a792f0cd2a528d983d5ac945772a740b2d253
Vladimir Yakushev <ru.phoenix@gmail.com>
2014-12-22 20:26:31 +0500

    Переход на Gradle

---
+++ bible/src/main/java/com/BibleQuote/BibleQuoteApp.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2011 Scripture Software (http://scripturesoftware.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.BibleQuote;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import com.BibleQuote.async.AsyncManager;
+import com.BibleQuote.managers.Librarian;
+import com.BibleQuote.managers.bookmarks.repository.IBookmarksRepository;
+import com.BibleQuote.managers.bookmarks.repository.dbBookmarksRepository;
+import com.BibleQuote.utils.Log;
+import com.BibleQuote.utils.PreferenceHelper;
+import com.BibleQuote.utils.UpdateManager;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+public class BibleQuoteApp extends Application {
+
+	private static final String TAG = "BibleQuoteApp";
+	private static BibleQuoteApp instance;
+
+	private Librarian myLibrarian;
+	private AsyncManager mAsyncManager;
+
+	public static String getAppVersionName(Context context) {
+		try {
+			return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+		} catch (PackageManager.NameNotFoundException e) {
+			return "0.00.01";
+		}
+	}
+
+	public static BibleQuoteApp getInstance() {
+		return instance;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instance = this;
+	}
+
+	public void Init() {
+		Log.i(TAG, "Init application preference helper...");
+		initPrefernceHelper();
+		Log.i(TAG, "Start update manager...");
+		UpdateManager.Init(this);
+		if (myLibrarian == null) {
+			Log.i(TAG, "Init library...");
+			initLibrarian();
+		}
+	}
+
+	public Librarian getLibrarian() {
+		if (myLibrarian == null) {
+			// Сборщик мусора уничтожил ссылки на myLibrarian и на PreferenceHelper
+			// Восстановим ссылки
+			initPrefernceHelper();
+			initLibrarian();
+		}
+		return myLibrarian;
+	}
+
+	public AsyncManager getAsyncManager() {
+		if (mAsyncManager == null) {
+			mAsyncManager = new AsyncManager();
+		}
+		return mAsyncManager;
+	}
+
+	public IBookmarksRepository getBookmarksRepository() {
+		//return new prefBookmarksRepository();
+		return new dbBookmarksRepository();
+	}
+
+	private void initPrefernceHelper() {
+		PreferenceHelper.Init(this);
+	}
+
+	private void initLibrarian() {
+		myLibrarian = new Librarian(this);
+	}
+
+	public synchronized Tracker getTracker() {
+		GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+		Tracker tracker = analytics.newTracker(R.xml.analitics);
+		tracker.enableAdvertisingIdCollection(true);
+		return tracker;
+	}
+}
---
+++ bible/src/main/java/com/BibleQuote/managers/GoogleAnalyticsHelper.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2011 Scripture Software (http://scripturesoftware.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.BibleQuote.managers;
+
+import android.content.Context;
+import com.BibleQuote.BibleQuoteApp;
+import com.BibleQuote.entity.BibleReference;
+import com.BibleQuote.managers.bookmarks.Bookmark;
+import com.BibleQuote.ui.ReaderActivity;
+import com.google.android.gms.analytics.HitBuilders;
+
+/**
+ * @author Vladimir Yakushev
+ * @version 1.0
+ */
+public class GoogleAnalyticsHelper {
+    private final static String CAATEGORY_BOOKMARKS = "bookmarks";
+    private static final String CATEGORY_MODULES = "modules";
+    private static final String CATEGORY_SEARCH = "search";
+    private static final String CATEGORY_PARALLELS = "parallels";
+    private static final String CATEGORY_HISTORY = "history";
+
+    private static volatile GoogleAnalyticsHelper instance;
+
+    private GoogleAnalyticsHelper() {
+    }
+
+    public static GoogleAnalyticsHelper getInstance() {
+        if (instance == null) {
+            synchronized (GoogleAnalyticsHelper.class) {
+                if (instance == null) {
+                    instance = new GoogleAnalyticsHelper();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void actionSendBookmark(Bookmark bookmark) {
+        for(String tag : bookmark.tags.split(",")) {
+            if (!"".equals(tag)) {
+                createEvent(CAATEGORY_BOOKMARKS, "tags", tag);
+            }
+        }
+        createEvent(CAATEGORY_BOOKMARKS, "bookmark", bookmark.OSISLink);
+    }
+
+    public void actionOpenLink(BibleReference reference, int openCode) {
+        switch (openCode) {
+            case ReaderActivity.ID_BOOKMARKS:
+                actionOpenBookmark(reference);
+                break;
+            case ReaderActivity.ID_CHOOSE_CH:
+                actionOpenChapter(reference);
+                break;
+            case ReaderActivity.ID_HISTORY:
+                actionOpenHistory(reference);
+                break;
+            case ReaderActivity.ID_PARALLELS:
+                actionOpenParallels(reference);
+                break;
+            case ReaderActivity.ID_SEARCH:
+                actionOpenSearchReference(reference);
+                break;
+        }
+    }
+
+    private void actionOpenSearchReference(BibleReference reference) {
+        createEvent(CATEGORY_SEARCH, "open", reference.getPath());
+    }
+
+    private void actionOpenParallels(BibleReference reference) {
+        createEvent(CATEGORY_PARALLELS, "open", reference.getPath());
+    }
+
+    private void actionOpenHistory(BibleReference reference) {
+        createEvent(CATEGORY_HISTORY, "open", reference.getPath());
+    }
+
+    public void actionOpenBookmark(BibleReference osisLink) {
+        createEvent(CAATEGORY_BOOKMARKS, "open", osisLink.getModuleID());
+    }
+
+    public void actionOpenChapter(BibleReference reference) {
+        createEvent(CATEGORY_MODULES, "open_module", reference.getModuleID());
+        createEvent(CATEGORY_MODULES, "open_book", reference.getBookID());
+    }
+
+    public void actionSearch(String moduleID, String query) {
+        createEvent(CATEGORY_SEARCH, moduleID, query);
+    }
+
+    private void createEvent(String category, String action, String label) {
+        BibleQuoteApp.getInstance().getTracker().send(new HitBuilders.EventBuilder()
+                .setCategory(category)
+                .setAction(action)
+                .setLabel(label)
+                .build());
+    }
+}
