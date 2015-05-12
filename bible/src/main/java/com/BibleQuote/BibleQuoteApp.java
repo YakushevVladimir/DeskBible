5cfbe1ee5f763f4141144674f37c24387d99aa8d
Vladimir Yakushev <ru.phoenix@gmail.com>
2015-05-12 15:21:05 +0500

    Значки панелей: - значки для xxhdpi и xxxhdpi - удалил значки для ldpi - значок для загрузки модуля с устройства

--- bible/src/main/java/com/BibleQuote/BibleQuoteApp.java
+++ bible/src/main/java/com/BibleQuote/BibleQuoteApp.java
@@ -1,23 +1,33 @@
 /*
- * Copyright (C) 2011 Scripture Software (http://scripturesoftware.org/)
+ * Copyright (c) 2011-2015. Scripture Software
+ * http://www.scripturesoftware.org
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * Project: BibleQuote
+ * File: BibleQuoteApp.java
+ *
+ * Created by Vladimir Yakushev at 5/2015
+ * E-mail: ru.phoenix@gmail.com
  */
 package com.BibleQuote;
 
 import android.app.Application;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import com.BibleQuote.async.AsyncManager;
 import com.BibleQuote.managers.Librarian;
 import com.BibleQuote.managers.bookmarks.repository.IBookmarksRepository;
@@ -75,7 +85,6 @@ public class BibleQuoteApp extends Application {
 	}
 
 	public IBookmarksRepository getBookmarksRepository() {
-		//return new prefBookmarksRepository();
 		return new dbBookmarksRepository();
 	}
 
