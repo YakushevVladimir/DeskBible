613648fee47016ae91b04576ead4953e733ccf5c
Vladimir Yakushev <ru.phoenix@gmail.com>
2016-10-07 23:46:23 +0500

    Matherial theme for BookmarkActivity

--- bible/src/main/java/com/BibleQuote/ui/BookmarksActivity.java
+++ bible/src/main/java/com/BibleQuote/ui/BookmarksActivity.java
@@ -30,19 +30,24 @@ package com.BibleQuote.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TabHost;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.BibleQuote.R;
 import com.BibleQuote.domain.entity.Bookmark;
 import com.BibleQuote.domain.entity.Tag;
-import com.BibleQuote.ui.adapters.TabsAdapter;
 import com.BibleQuote.ui.fragments.BookmarksFragment;
 import com.BibleQuote.ui.fragments.TagsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookmarksActivity extends AppCompatActivity
         implements BookmarksFragment.OnBookmarksChangeListener, TagsFragment.OnTagsChangeListener {
@@ -50,37 +55,59 @@ public class BookmarksActivity extends AppCompatActivity
     public static final String EXTRA_MODE = "extra_mode";
     public static final String MODE_TAGS = "tags";
     public static final String MODE_BOOKMARKS = "bookmarks";
+
     private static final String KEY_TAB = "tab";
-    private TabHost mTabHost;
-    private TabsAdapter mTabsAdapter;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bookmarks_activity);
 
-        mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addPage(getString(R.string.bookmarks), new BookmarksFragment());
+        pagerAdapter.addPage(getString(R.string.tags), new TagsFragment());
 
-        mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
-        mTabsAdapter.addTab(mTabHost.newTabSpec(MODE_BOOKMARKS).setIndicator(getTabIndicator(R.string.bookmarks)),
-                new BookmarksFragment(), null);
-        mTabsAdapter.addTab(mTabHost.newTabSpec(MODE_TAGS).setIndicator(getTabIndicator(R.string.tags)),
-                new TagsFragment(), null);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
 
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(EXTRA_MODE)) {
-            mTabHost.setCurrentTabByTag(intent.getStringExtra(EXTRA_MODE));
+            final String mode = intent.getStringExtra(EXTRA_MODE);
+            viewPager.setCurrentItem(mode.equals(MODE_BOOKMARKS) ? 0 : 1);
         } else if (savedInstanceState != null) {
-            mTabHost.setCurrentTabByTag(savedInstanceState.getString(KEY_TAB));
+            viewPager.setCurrentItem(savedInstanceState.getInt(KEY_TAB));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(KEY_TAB, mTabHost.getCurrentTabTag());
+        outState.putInt(KEY_TAB, viewPager.getCurrentItem());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+        viewPager.setCurrentItem(state.getInt(KEY_TAB));
     }
 
     @Override
@@ -93,24 +120,54 @@ public class BookmarksActivity extends AppCompatActivity
 
     @Override
     public void onBookmarksUpdate() {
-        ((TagsFragment) mTabsAdapter.getItem(1)).updateTags();
+        ((TagsFragment) pagerAdapter.getItem(1)).updateTags();
     }
 
     @Override
     public void onTagSelect(Tag tag) {
-        mTabHost.setCurrentTab(0);
-        ((BookmarksFragment) mTabsAdapter.getItem(0)).setTagFilter(tag);
+        viewPager.setCurrentItem(0);
+        ((BookmarksFragment) pagerAdapter.getItem(0)).setTagFilter(tag);
     }
 
     @Override
     public void onTagsUpdate() {
-        ((BookmarksFragment) mTabsAdapter.getItem(0)).updateBookmarks();
+        ((BookmarksFragment) pagerAdapter.getItem(0)).updateBookmarks();
     }
 
-    private View getTabIndicator(int stringID) {
-        View tabView = LayoutInflater.from(this).inflate(R.layout.tab_indicator, mTabHost, false);
-        TextView tv = (TextView) tabView.findViewById(R.id.tab);
-        tv.setText(getResources().getText(stringID).toString().toUpperCase());
-        return tabView;
+    private static class PagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> pages = new ArrayList<Fragment>();
+        private List<String> titles = new ArrayList<String>();
+
+        PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return pages.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (pages.size() > position) {
+                return pages.get(position);
+            }
+            return new Fragment();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (titles.size() > position) {
+                return titles.get(position);
+            }
+            return "";
+        }
+
+        void addPage(String name, Fragment page) {
+            titles.add(name);
+            pages.add(page);
+        }
+
     }
 }
