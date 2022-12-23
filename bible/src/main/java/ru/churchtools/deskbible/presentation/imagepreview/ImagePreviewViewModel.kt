f7d5b9f0737df1cbf94e1f2c2e7a1cfa925e6a51
Александр <alexwot1212@yandex.ru>
2022-12-23 16:45:49 +0300

    Add method onActivityCreate

--- bible/src/main/java/ru/churchtools/deskbible/presentation/imagepreview/ImagePreviewViewModel.kt
+++ bible/src/main/java/ru/churchtools/deskbible/presentation/imagepreview/ImagePreviewViewModel.kt
@@ -1,5 +1,8 @@
 package ru.churchtools.deskbible.presentation.imagepreview
 
+import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.BibleQuote.di.scope.PerActivity
@@ -10,6 +13,17 @@ class ImagePreviewViewModel(
     private val myLibrarian: Librarian,
 ) : ViewModel() {
 
+    private val _imageState = MutableLiveData<ImagePreviewViewResult>()
+    val imageState: LiveData<ImagePreviewViewResult>
+        get() = _imageState
+
+    private lateinit var imageBitmap: Bitmap
+    private val IMAGE_PATH: String = "image_path"
+
+    fun onActivityCreate() {
+        imageBitmap = myLibrarian.getModuleImage(IMAGE_PATH)
+        _imageState.value = ImagePreviewViewResult.DrawImage(imageBitmap)
+    }
 
     @PerActivity
     class Factory @Inject constructor(
