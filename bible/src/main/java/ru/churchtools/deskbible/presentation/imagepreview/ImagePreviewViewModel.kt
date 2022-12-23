39e40654adc4adbbf486ed2dd8236097b74624ab
Александр <alexwot1212@yandex.ru>
2022-12-23 13:24:36 +0300

    Add class Factory use Dagger2 in ViewModel

--- bible/src/main/java/ru/churchtools/deskbible/presentation/imagepreview/ImagePreviewViewModel.kt
+++ bible/src/main/java/ru/churchtools/deskbible/presentation/imagepreview/ImagePreviewViewModel.kt
@@ -1,11 +1,22 @@
 package ru.churchtools.deskbible.presentation.imagepreview
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.BibleQuote.di.scope.PerActivity
 import com.BibleQuote.managers.Librarian
+import javax.inject.Inject
 
 class ImagePreviewViewModel(
     private val myLibrarian: Librarian,
 ) : ViewModel() {
 
 
+    @PerActivity
+    class Factory @Inject constructor(
+        private val myLibrarian: Librarian,
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            ImagePreviewViewModel(myLibrarian) as T
+    }
 }
